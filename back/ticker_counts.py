13465ac0c5dfb76fffb2f0b04e86b1686776461e
Victor Uriarte <victor.m.uriarte@intel.com>
2021-02-11 10:46:43 -0700

    Consistently use single quotes

--- back/ticker_counts.py
+++ back/ticker_counts.py
@@ -30,14 +30,14 @@ class TickerCounts:
             pass
         return False
 
-    def extract_ticker(self, body: str, re_string: str = "[$][A-Za-z]*|[A-Z][A-Z]{1,}") -> Set[str]:
+    def extract_ticker(self, body: str, re_string: str = r'[$][A-Za-z]*|[A-Z][A-Z]{1,}') -> Set[str]:
         """Simple Regex to get tickers from text."""
         ticks = set(re.findall(re_string, str(body)))
         res = set()
         for item in ticks:
             if item not in self.block_words and item.lower() not in self.stop_words and item:
                 try:
-                    tic = item.replace("$", "").upper()
+                    tic = item.replace('$', '').upper()
                     res.add(tic)
                 except Exception as e:
                     print(e)
@@ -47,7 +47,7 @@ class TickerCounts:
         # Scrape subreddits `r/robinhoodpennystocks` and `r/pennystocks`
         # Current it does fetch a lot of additional data like upvotes, comments, awards etc but not using anything apart from title for now
         reddit = praw.Reddit('ClientSecrets')
-        subreddits = "+".join(json.loads(self.config['FilteringOptions']['Subreddits']))
+        subreddits = '+'.join(json.loads(self.config['FilteringOptions']['Subreddits']))
         new_bets = reddit.subreddit(subreddits).new(limit=self.WEBSCRAPER_LIMIT)
 
         posts = [
@@ -58,41 +58,41 @@ class TickerCounts:
                 post.num_comments,
                 post.upvote_ratio,
                 post.total_awards_received
-            ] for post in tqdm(new_bets, desc="Selecting relevant data from webscraper", total=self.WEBSCRAPER_LIMIT)
+            ] for post in tqdm(new_bets, desc='Selecting relevant data from webscraper', total=self.WEBSCRAPER_LIMIT)
         ]
-        posts = pd.DataFrame(posts, columns=["id",
-                                             "title",
-                                             "score",
-                                             "comments",
-                                             "upvote_ratio",
-                                             "total_awards"])
+        posts = pd.DataFrame(posts, columns=['id',
+                                             'title',
+                                             'score',
+                                             'comments',
+                                             'upvote_ratio',
+                                             'total_awards'])
 
         # Extract tickers from all titles and create a new column
-        posts["Tickers"] = posts["title"].apply(self.extract_ticker)
+        posts['Tickers'] = posts['title'].apply(self.extract_ticker)
         ticker_sets = posts.Tickers.to_list()
 
         # Count number of occurrences of the Ticker and verify id the Ticker exists
         counts = reduce(add, map(Counter, ticker_sets))
 
         verified_tics = {}
-        for ticker, ticker_count in tqdm(counts.items(), desc="Filtering verified ticks"):
+        for ticker, ticker_count in tqdm(counts.items(), desc='Filtering verified ticks'):
             # If ticker is found more than 3 times and ticker is valid
             if ticker_count > 3 and self.verify_ticker(ticker):
                 verified_tics[ticker] = ticker_count
 
         # Create Datable of just mentions
-        tick_df = pd.DataFrame(verified_tics.items(), columns=["Ticker", "Mentions"])
-        tick_df.sort_values(by=["Mentions"], inplace=True, ascending=False)
+        tick_df = pd.DataFrame(verified_tics.items(), columns=['Ticker', 'Mentions'])
+        tick_df.sort_values(by=['Mentions'], inplace=True, ascending=False)
         tick_df.reset_index(inplace=True, drop=True)
 
         date_created = datetime.today().strftime('%Y-%m-%d')
-        csv_filename = f"{date_created}_tick_df"
-        directory_output = "./data"
+        csv_filename = f'{date_created}_tick_df'
+        directory_output = './data'
 
         if not os.path.exists(directory_output):
             os.mkdir(directory_output)
 
-        full_output_path = f"{directory_output}/{csv_filename}.csv"
+        full_output_path = f'{directory_output}/{csv_filename}.csv'
         tick_df.to_csv(full_output_path, index=False)
         print(tick_df.head())
 
--- back/yfinance_analysis.py
+++ back/yfinance_analysis.py
@@ -10,22 +10,22 @@ class FinanceAnalysis:
     def analyze(self, best_n=25):
         # Load data from file, generate data by running the `ticker_counts.py` script
         date_created = datetime.today().strftime('%Y-%m-%d')
-        csv_filename = f"{date_created}_tick_df"
-        data_directory = "./data"
+        csv_filename = f'{date_created}_tick_df'
+        data_directory = './data'
 
-        full_input_path = f"{data_directory}/{csv_filename}.csv"
+        full_input_path = f'{data_directory}/{csv_filename}.csv'
 
-        tick_df = pd.read_csv(full_input_path).sort_values(by=["Mentions", "Ticker"], ascending=False)
+        tick_df = pd.read_csv(full_input_path).sort_values(by=['Mentions', 'Ticker'], ascending=False)
         tick_df.dropna(axis=1)
 
-        dataColumns = ["Name", "Industry", "Previous Close", "5d Low", "5d High", "1d Change (%)", "5d Change (%)",
-                       "1mo Change (%)"]
+        dataColumns = ['Name', 'Industry', 'Previous Close', '5d Low', '5d High', '1d Change (%)', '5d Change (%)',
+                       '1mo Change (%)']
         df_best = tick_df.head(best_n)
         df_best[dataColumns] = df_best.Ticker.apply(self.get_ticker_info)
 
         # Save to file to load into yahoo analysis script
-        csv_filename = f"df_best_{best_n}"
-        full_output_path = f"{data_directory}/{csv_filename}.csv"
+        csv_filename = f'df_best_{best_n}'
+        full_output_path = f'{data_directory}/{csv_filename}.csv'
 
         df_best.to_csv(full_output_path, index=False)
         print(df_best.head())
@@ -34,30 +34,30 @@ class FinanceAnalysis:
         """Use Yahoo Finance API to get the relevant data."""
         return round(((end - start) / start) * 100, 2)
 
-    def get_change(self, ticker: str, period: str = "1d") -> float:
+    def get_change(self, ticker: str, period: str = '1d') -> float:
         return self.calculate_change(
-            yf.Ticker(ticker).history(period)["Open"].to_list()[0],
-            yf.Ticker(ticker).history(period)["Close"].to_list()[-1]
+            yf.Ticker(ticker).history(period)['Open'].to_list()[0],
+            yf.Ticker(ticker).history(period)['Close'].to_list()[-1]
         )
 
     def get_ticker_info(self, ticker):
         # Standard Data
         info = yf.Ticker(ticker).info
-        tickerName = info.get("longName")
-        tickerIndustry = info.get("industry")
+        tickerName = info.get('longName')
+        tickerIndustry = info.get('industry')
 
         # previous Day close
-        tickerClose = yf.Ticker(ticker).history(period="1d")["Close"].to_list()[-1]
+        tickerClose = yf.Ticker(ticker).history(period='1d')['Close'].to_list()[-1]
 
         # Highs and Lows
-        highLow = yf.Ticker(ticker).history(period="5d")
-        Low5d = min(highLow["Low"].to_list())
-        High5d = max(highLow["High"].to_list())
+        highLow = yf.Ticker(ticker).history(period='5d')
+        Low5d = min(highLow['Low'].to_list())
+        High5d = max(highLow['High'].to_list())
 
         # Changes
         change1d = self.get_change(ticker)
-        change5d = self.get_change(ticker, "5d")
-        change1mo = self.get_change(ticker, "1mo")
+        change5d = self.get_change(ticker, '5d')
+        change1mo = self.get_change(ticker, '1mo')
 
         return pd.Series([tickerName, tickerIndustry, tickerClose, Low5d, High5d, change1d, change5d, change1mo])
 
