40ed810a1846371137e279c69e9a807cac40f7dd
Tyler Barham <tyler.barham@tomra.com>
2021-02-11 14:58:42 +1300

    Update backend data before starting the flask app

--- back/wsgi.py
+++ back/wsgi.py
@@ -2,7 +2,11 @@ from ticker_counts import main as update_reddit_mentions
 from yfinance_analysis import main as update_financial_data
 from server import app
 
-update_reddit_mentions()
-update_financial_data()
+def main():
+    update_reddit_mentions()
+    update_financial_data()
 
-app.run(host='0.0.0.0', port=5000)
+    app.run(host='0.0.0.0', port=5000)
+
+if __name__ == '__main__':
+    main()
