ea29dc3e4e72ac51d01f50ccd673f493ea9c86d2
Martin <office@martinpagels.de>
2021-02-21 00:30:58 +0100

    Finalize Global Styles

--- react-front/src/GlobalStyles.js
+++ react-front/src/GlobalStyles.js
@@ -4,21 +4,22 @@ export default createGlobalStyle`
 
 * {
     box-sizing: border-box;
-    font-family: 'BaiJamjuree';
+    
 }
 
 body {
     
     margin: 0;
-    font-size: 112.5%;
+    font-size: 100%;
     background-color: var(--surface-a);
-    font-family: var(--bs-font-sans-serif);
+    font-family: 'Helvetica Neue', sans-serif;
+
 }
 
+a {
+    color: white;
+}
 :root {
     --surface-a: #1f2d40;
---bs-font-sans-serif: system-ui,-apple-system,"Segoe UI",Roboto,"Helvetica Neue",Arial,"Noto Sans","Liberation Sans",sans-serif,"Apple Color Emoji","Segoe UI Emoji","Segoe UI Symbol","Noto Color Emoji";
---bs-font-monospace: SFMono-Regular,Menlo,Monaco,Consolas,"Liberation Mono","Courier New",monospace;
---bs-gradient: linear-gradient(180deg, rgba(255, 255, 255, 0.15), rgba(255, 255, 255, 0));
     }
 `;
